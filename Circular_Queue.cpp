a78eea1c04c23dbe41fc49e64faee4fc9d8558bb
sbshafayet <41725521+sbshafayet@users.noreply.github.com>
2018-07-26 14:54:28 +0600

    Update Circular_Queue.cpp

--- Circular_Queue.cpp
+++ Circular_Queue.cpp
@@ -1,71 +1,95 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-#define N 5
-class Queue{
-private:
-    int front, rear;
-    int q[N];
-public:
-    Queue()
-    {
-        front=0;
-        rear=-1;
-    }
-    bool is_empty()
-    {
-        int empty=0;
-        if(front==rear+1)
-            empty=1;
-        return empty;
-    }
-    bool is_full()
-    {
-        int full=0;
-        if(rear==N-1)
-            full=1;
-        return full;
-    }
-    void en_queue(int x)
-    {
-        if(is_full())
+#define MAX 5
+class Queue
+{
+    private:
+        int front,rear;
+        int ele[MAX];
+    public:
+        Queue()
         {
-            cout<<"\nQueue overflow."<<endl;
-            return;
+            front =  0;
+            rear  = -1;
         }
-        q[++rear]=x;
-        cout<<"\nInserted value: "<<x;
-    }
-    int de_queue( int *x)
-    {
-        if(is_empty())
-        {
-            cout<<"\nQueue underflow."<<endl;
-            return -1;
-        }
-        *x=q[front++];
-        return 0;
-    }
+
+        int  isFull();
+        int  isEmpty();
+        void insertQueue(int  item);
+        int  deleteQueue(int *item);
 };
+int Queue::isFull()
+{
+    int full = 0 ;
+
+    if( rear == MAX-1 )
+        full = 1;
+
+    return full;
+}
+int Queue::isEmpty()
+{
+    int empty = 0 ;
+
+    if( front == rear + 1 )
+        empty = 1;
+
+    return empty;
+}
+void Queue:: insertQueue(int item)
+{
+    if( isFull() )
+    {
+        cout<<"\nQueue OverFlow" << endl;
+        return;
+    }
+
+    ele[++rear]=item;
+    cout<<"\ninserted Value :" << item;
+}
+int Queue:: deleteQueue(int *item)
+{
+    if( isEmpty() )
+    {
+        cout<<"\nQueue Underflow" << endl;
+        return -1;
+    }
+
+    *item = ele[front++];
+    return 0;
+}
+
 int main()
 {
-    int x=0;
-    Queue q=Queue();
-    q.en_queue(10);
-    q.en_queue(20);
-    q.en_queue(30);
-    q.en_queue(40);
-    q.en_queue(50);
+    int item=0;
 
-    if(q.de_queue(&x)==0)
-        cout<<"\nDeleted item: "<<x;
-    if(q.de_queue(&x)==0)
-        cout<<"\nDeleted item: "<<x;
-    if(q.de_queue(&x)==0)
-        cout<<"\nDeleted item: "<<x;
-    if(q.de_queue(&x)==0)
-        cout<<"\nDeleted item: "<<x;
-    if(q.de_queue(&x)==0)
-        cout<<"\nDeleted item: "<<x;
-    cout<<endl;
+    Queue q = Queue();
+
+    q.insertQueue(10);
+    q.insertQueue(20);
+    q.insertQueue(30);
+    q.insertQueue(40);
+    q.insertQueue(50);
+    q.insertQueue(60);
+
+    if(q.deleteQueue( &item)==0)
+        cout<<"\nDeleted item : "<< item;
+
+    if(q.deleteQueue( &item)==0)
+        cout<<"\nDeleted item : "<< item;
+
+    if(q.deleteQueue( &item)==0)
+        cout<<"\nDeleted item : "<< item;
+
+    if(q.deleteQueue( &item)==0)
+        cout<<"\nDeleted item : "<< item;
+
+    if(q.deleteQueue( &item)==0)
+        cout<<"\nDeleted item : "<< item;
+
+    if(q.deleteQueue( &item)==0)
+        cout<<"\nDeleted item : "<< item;
+
+    cout<< endl;
     return 0;
 }
