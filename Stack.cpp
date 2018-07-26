9bbc9bda815c8f570391042f9556772ff31ca5c5
sbshafayet <41725521+sbshafayet@users.noreply.github.com>
2018-07-26 14:13:08 +0600

    Create Stack.cpp
    Stack with Class

---
+++ Stack.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+class stack
+{
+private:
+        char a[10];
+        int top;
+
+public:
+        stack();
+        ~stack();
+        void push(char ch);
+        char pop();
+        void show();
+};
+stack::stack()
+{
+    top = 0;
+}
+
+stack::~stack()
+{
+    cout<<"\ndestructor\n";
+}
+
+void stack::push(char ch)
+{
+    if(top == 10)
+	{
+	  cout << "Stack is full";
+		return ;
+	}
+	a[top] = ch;
+	top++;
+}
+
+char stack::pop()
+{
+    if(top == 0)
+	{
+		cout << "Stack is empty";
+		return 0;
+	}
+	top--;
+	return a[top];
+}
+
+void stack::show()
+{
+    int i;
+    cout<<"\nStack \n";
+    for(i=0;i<=top;i++)
+    cout<<"\n"<<pop()<<"\n";
+}
+
+int main()
+{
+	stack s1;
+	s1.push('a');
+	s1.push('b');
+	s1.push('c');
+	s1.push('d');
+	//s1.pop();
+	s1.show();
+	s1.pop();
+	return 0;
+
+}
